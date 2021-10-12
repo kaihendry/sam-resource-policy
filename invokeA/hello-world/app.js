a2622dbc9c5c992258f59cfb92464194a8d5f848
Kai Hendry <hendry@iki.fi>
2021-10-12 15:23:59 +0800

    Sign seperately

--- invokeA/hello-world/app.js
+++ invokeA/hello-world/app.js
@@ -1,8 +1,10 @@
 const AWSXRay = require("aws-xray-sdk-core");
 AWSXRay.captureHTTPsGlobal(require("https"));
 
+const https = require("https");
 const axios = require("axios");
 const aws4 = require("aws4");
+
 let response;
 
 exports.lambdaHandler = async (event, context) => {
@@ -13,33 +15,23 @@ exports.lambdaHandler = async (event, context) => {
     method: "GET",
     url: "https://ytecj8f77e.execute-api.ap-southeast-1.amazonaws.com/Prod/",
   };
+  aws4.sign(request);
+  console.log(request);
 
-  try {
-    let result = await axios(request);
-    console.log(JSON.stringify(result.data));
-    response = {
-      statusCode: 200,
-      body: JSON.stringify(result.data),
-    };
-  } catch (error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-      // http.ClientRequest in node.js
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("Error", error.message);
-    }
-    console.log(error.config);
-    // What response should we return?
-    return error;
-  }
-  return response;
+  let data = [];
+  return new Promise((resolve, reject) => {
+    const req = https.request(request, (res) => {
+      console.log("statusCode:", res.statusCode);
+      console.log("headers:", res.headers);
+      res.on("data", (d) => {
+        console.log("Data: ", d.toString());
+        resolve(d.toString());
+      });
+    });
+    req.on("error", (err) => {
+      console.log("Error: ", err);
+      reject(err);
+    });
+    req.end();
+  });
 };
