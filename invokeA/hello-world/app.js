a3927b7dae0534b77a963622b82b0b94f7d7ed89
Kai Hendry <hendry@iki.fi>
2021-10-14 09:18:40 +0800

    With AXIOS

--- invokeA/hello-world/app.js
+++ invokeA/hello-world/app.js
@@ -1,37 +1,41 @@
 const AWSXRay = require("aws-xray-sdk-core");
 AWSXRay.captureHTTPsGlobal(require("https"));
 
-const https = require("https");
+const axios = require("axios");
 const aws4 = require("aws4");
 
 let response;
 
 exports.lambdaHandler = async (event, context) => {
-  console.log(process.env);
+  console.log(event, context, process.env);
 
-  // let request = aws4.sign({
   let request = {
     method: "GET",
+    url: "https://ytecj8f77e.execute-api.ap-southeast-1.amazonaws.com/Prod/",
     host: "ytecj8f77e.execute-api.ap-southeast-1.amazonaws.com",
-    path: "/Prod"
+    path: "/Prod",
   };
+
   aws4.sign(request);
+  // AWS signature is now in the headers
   console.log(request);
 
-  let data = [];
-  return new Promise((resolve, reject) => {
-    const req = https.request(request, (res) => {
-      console.log("statusCode:", res.statusCode);
-      console.log("headers:", res.headers);
-      res.on("data", (d) => {
-        console.log("Data: ", d.toString());
-        resolve(d.toString());
-      });
-    });
-    req.on("error", (err) => {
-      console.log("Error: ", err);
-      reject(err);
-    });
-    req.end();
-  });
+  try {
+    const ret = await axios(request);
+
+    response = {
+      statusCode: ret.status,
+      body: JSON.stringify({
+        message: ret.data,
+      }),
+    };
+  } catch (error) {
+    response = {
+      statusCode: error.response?.status || 501,
+      body: JSON.stringify({
+        message: error.message,
+      }),
+    };
+  }
+  return response;
 };
